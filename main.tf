615d0912d3bf7ecfbbab5bd5258cac3855f096c0
Aladdin Almubayed <aladdin.almubayed@resourcely.io>
2025-03-06 11:33:45 -0800

    Update versioning_configuration status in main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 # main.tf
 
-resource "aws_s3_bucket" "resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA" {
-  bucket = "resourcely-campaigns-test-bucket"
+resource "aws_s3_bucket" "resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA" {
+  bucket = "resourcely-campaigns-test-bucketeisbrjszjfgsdgmh"
 }
 
-resource "aws_s3_bucket_public_access_block" "resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA" {
-  bucket                  = aws_s3_bucket.resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_public_access_block" "resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA" {
+  bucket                  = aws_s3_bucket.resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA.id
   block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA" {
-  bucket = aws_s3_bucket.resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_ownership_controls" "resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA" {
+  bucket = aws_s3_bucket.resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_versioning" "resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA" {
-  bucket = aws_s3_bucket.resourcely-campaigns-test-bucket_ckaMshbmm94dxCHA.id
+resource "aws_s3_bucket_versioning" "resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA" {
+  bucket = aws_s3_bucket.resourcely-campaigns-test-bucket_eisbrjszjfgsdgmhMshbmm94dxCHA.id
 
   versioning_configuration {
     status = "Enabled"
