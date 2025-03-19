d27d0e8867cc2ff37fc393adbde588f87ca882c3
Aladdin Almubayed <aladdin.almubayed@resourcely.io>
2025-03-19 11:13:34 -0700

    Update versioning_configuration status in main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 resource "aws_s3_bucket" "campaigns-resource_ckaMshbmm94dxCHA" {
-  bucket = "campaigns-resource-test-bucket"
+  bucket = "campaigns-resource-test-bucket-ranrpmubjeedkqlk"
 }
 
 resource "aws_s3_bucket_public_access_block" "campaigns-resource_ckaMshbmm94dxCHA" {
